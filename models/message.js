84673a75d8242fba1c885e45fdce2941db3dd74c
Fatih Arapoglu <f3tt4n@gmail.com>
2023-03-04 03:12:57 +0300

    defined models for user and message

---
+++ models/message.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema({
+    title: {
+        type: String,
+    },
+    text: {
+        type: String,
+        required: true,
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now,
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+});
+
+module.exports = mongoose.model("Message", MessageSchema);
---
+++ models/user.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        maxLength: [20, "Name should be lower than 20 characters"],
+    },
+    surname: {
+        type: String,
+        required: true,
+        maxLength: [20, "Last name should be lower than 20 characters"],
+    },
+    email: {
+        type: String,
+        required: true,
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            "Email is not valid",
+        ],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: [6, "Password should be at least 6 characters"],
+    },
+    isMember: {
+        type: Boolean,
+        default: true,
+    },
+});
+
+UserSchema.virtual("fullname").get(function () {
+    return `${this.name} ${this.surname}`;
+});
+
+module.exports = mongoose.model("User", UserSchema);
