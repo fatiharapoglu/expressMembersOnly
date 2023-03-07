898f30899782929e86eecab9ef1c0aecad217a3a
Fatih Arapoglu <f3tt4n@gmail.com>
2023-03-07 05:02:46 +0300

    add error view template

--- controllers/sign-up.js
+++ controllers/sign-up.js
@@ -2,7 +2,6 @@ const User = require("../models/user");
 
 const signUp = async (req, res) => {
     try {
-        console.log(req.body);
         const user = await User.create(req.body);
         res.status(201).render("home");
     } catch (error) {
--- models/user.js
+++ models/user.js
@@ -20,7 +20,7 @@ const UserSchema = new Schema({
             /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
             "Email is not valid",
         ],
-        unique: true,
+        unique: [true, "This e-mail is already in use"],
     },
     password: {
         type: String,
