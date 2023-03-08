e2fca5e4f9b3d1ad109ec55a04fba9f5aaf3faf7
Fatih Arapoglu <f3tt4n@gmail.com>
2023-03-08 03:29:10 +0300

    add fully upgradable premium status

--- controllers/get-premium.js
+++ controllers/get-premium.js
@@ -1,10 +1,14 @@
 const User = require("../models/user");
 
 const getPremiumController = async (req, res) => {
-    if (req.body.hidden === process.env.HIDDEN_PASSWORD) {
-        return res.send("Correct");
+    if (req.body.hidden !== process.env.HIDDEN_PASSWORD) {
+        return res.status(401).render("error", {
+            err: "Wrong hidden password. Please get code from github repo's readme file...",
+        });
     }
-    res.send("nope");
+    const upgradingEmail = res.locals.currentUser.email;
+    await User.findOneAndUpdate({ email: upgradingEmail }, { isPremium: true }, { new: true });
+    res.redirect("/get-premium");
 };
 
 module.exports = getPremiumController;
--- routes/get-premium.js
+++ routes/get-premium.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const getPremiumController = require("../controllers/get-premium");
 
 router.get("/", (req, res) => {
+    if (!res.locals.currentUser) {
+        return res.status(401).render("error", { err: "Authorization error, please log in." });
+    }
     res.status(200).render("get-premium");
 });
 
